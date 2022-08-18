a2da754d7873f77930d4f771659c7269fd44ea8e
Samuel Paget <sjapaget@gmail.com>
2022-08-18 15:55:32 +0200

    Add functionalities: index lists, show list, add bookmark, delete bookmark

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
-  get 'lists/index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  # root "articles#index"
-  resources :lists, only: [:index, :show, :new, :create]
+
+  resources :lists, only: [:index, :new, :create, :show, :destroy] do
+    resources :bookmarks, only: [:new, :create, :destroy]
+  end
 end
