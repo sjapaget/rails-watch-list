efb997a5b7b66b64206b8e0e6edb4c9b52632156
Samuel Paget <sjapaget@gmail.com>
2022-08-19 15:41:49 +0200

    Add image upload to list creation

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -33,7 +33,7 @@ class ListsController < ApplicationController
   private
 
   def list_params
-    params.require(:list).permit(:name)
+    params.require(:list).permit(:name, :image)
   end
 
   def set_list
